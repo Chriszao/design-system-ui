04c41ca0a6450c3b7dd028c9c7c92517013806fa
Christofer Assis <chris.f.assis18@gmial.com>
2023-06-18 16:45:56 -0300

    fix: fix component displayName in storybook

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Box, BoxProps, Text } from '@design-system-ui/react';
 
 export default {
-	title: 'Box',
+	title: 'Surfaces/Box',
 	component: Box,
 	args: {
 		children: <Text>Testing Box Element</Text>,
