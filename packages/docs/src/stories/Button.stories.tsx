13b58f74c58b13cde518d5a0bd21e5ac3ea9043f
Christofer Assis <chris.f.assis18@gmial.com>
2023-06-18 15:10:22 -0300

    feat: add Button stories and component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,48 @@
+import { Button, ButtonProps } from '@design-system-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+	title: 'Form/Button',
+	component: Button,
+	args: { children: 'Send' },
+} as Meta<ButtonProps>;
+import { ArrowRight } from 'phosphor-react';
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Secondary: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'secondary',
+		children: 'Create new',
+	},
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'tertiary',
+		children: 'Cancel',
+	},
+};
+
+export const Small: StoryObj<ButtonProps> = {
+	args: {
+		size: 'sm',
+	},
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+	args: {
+		disabled: true,
+	},
+};
+
+export const WithIcon: StoryObj<ButtonProps> = {
+	args: {
+		children: (
+			<>
+				Next Step
+				<ArrowRight weight="bold" />
+			</>
+		),
+	},
+};
