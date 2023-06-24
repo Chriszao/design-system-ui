26806575337c69ad7743d965194c0c6fb6b90b49
Christofer Assis <chris.f.assis18@gmial.com>
2023-06-24 16:12:37 -0300

    fix: fix typescript error when using size props

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,10 +1,16 @@
 # @design-system-ui/react
 
+## 2.0.2
+
+### Patch Changes
+
+- fix typescript error when using size props in TextInput Component
+
 ## 2.0.1
 
 ### Patch Changes
 
-- Add option to pass reference to text imput and adjust some styles
+- Add option to pass reference to text input and adjust some styles
 
 ## 2.0.0
 
--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,14 +1,22 @@
 import { ComponentProps, ElementRef, forwardRef } from 'react';
 import { Input, Prefix, TextInputContainer } from './styles';
 
-export interface TextInputProps extends ComponentProps<typeof Input> {
+type TextInputSize = Pick<
+	ComponentProps<typeof TextInputContainer>,
+	'size'
+>['size'];
+
+type InputPropsWithoutSize = Omit<ComponentProps<typeof Input>, 'size'>;
+
+export interface TextInputProps extends InputPropsWithoutSize {
 	prefix?: string;
+	size?: TextInputSize;
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-	({ prefix, ...props }, ref) => {
+	({ prefix, size, ...props }, ref) => {
 		return (
-			<TextInputContainer>
+			<TextInputContainer size={size}>
 				{!!prefix && <Prefix>{prefix}</Prefix>}
 				<Input ref={ref} {...props} />
 			</TextInputContainer>
